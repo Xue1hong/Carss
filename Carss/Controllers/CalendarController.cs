60872a4bfa119cc979c885b152601e6ab4a4c3d2
yuxiang <111b15022@mailst.cjcu.edu.tw>
2024-10-28 22:10:58 +0800

    加入專案檔案。

---
+++ Carss/Controllers/CalendarController.cs
@@ -0,0 +1,73 @@
+﻿using Dapper;
+using Carss.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace Calendars.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalendarController : ControllerBase
+    {
+        private readonly string _connectString = DBUtil.ConnectionString(); [HttpGet]
+        public async Task<IEnumerable<Calendar>> GetCalendars(){
+            string sqlQuery = "SELECT * FROM VehicleRentals"; 
+            using (var connection = new SqlConnection(_connectString))
+
+            { 
+                var Calendars = await connection.QueryAsync<Calendar>(sqlQuery); 
+                return Calendars.ToList(); 
+            }
+        }
+
+        [HttpGet("{id}")] 
+        public async Task<Calendar> GetCalendar(Guid id) 
+        { 
+            string sqlQuery = "SELECT * FROM VehicleRentals WHERE RentalId = @Id"; 
+            using (var connection = new SqlConnection(_connectString)) 
+            { 
+                var Calendar = await connection.QueryFirstOrDefaultAsync<Calendar>(sqlQuery, new { Id = id }); 
+                if (Calendar == null) 
+                { 
+                    return new Calendar(); 
+                } 
+                return Calendar; 
+            } 
+        }
+
+        [HttpPost] 
+        public async Task<IActionResult> AddCalendar(Calendar Calendar) 
+        { 
+            string sqlQuery = "INSERT  INTO  VehicleRentals  (VehicleName,  CustomerName,  RentalDate, ReturnDate, IsReturned)  VALUES (@VehicleName,  @CustomerName,  @RentalDate, @ReturnDate, @IsReturned)"; 
+            using (var connection = new SqlConnection(_connectString)) 
+            { 
+                await connection.ExecuteAsync(sqlQuery, Calendar); 
+                return Ok(); 
+            } 
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCalendar(Calendar Calendar)
+        {
+            string sqlQuery = "UPDATE VehicleRentals SET VehicleName = @VehicleName, CustomerName = @CustomerName, RentalDate = @RentalDate, ReturnDate=@ReturnDate, IsReturned = @IsReturned, CreatedDate = @CreatedDate WHERE RentalId = @RentalId";
+            using (var connection = new SqlConnection(_connectString))
+            {
+                await connection.ExecuteAsync(sqlQuery, Calendar);
+                return Ok();
+
+            }
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCalendar(Guid id)
+        {
+            string sqlQuery = "DELETE FROM VehicleRentals WHERE RentalId = @Id";
+            using (var connection = new SqlConnection(_connectString))
+            {
+                await connection.ExecuteAsync(sqlQuery, new { Id = id });
+                return Ok();
+            }
+        }
+
+
+    }
+}
---
+++ Carss/Models/Calendar.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Carss.Models
+{
+    public class Calendar
+    {
+        [Key]
+        public Guid RentalId { get; set; }  
+
+        [Required]  
+        [MaxLength(50)]  
+        public string VehicleName { get; set; }  
+
+        [Required]  
+        [MaxLength(50)] 
+        public string CustomerName { get; set; }  
+
+        [Required]  
+        public DateTime RentalDate { get; set; }  
+
+        [Required] 
+        public DateTime ReturnDate { get; set; } 
+
+        [Required] 
+        public bool IsReturned { get; set; }  
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
